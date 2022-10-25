72a7eec9bb63b704b6eef56fa6a33cbc5d21d69d
arislid <arislid@gmail.com>
2022-10-25 06:39:45 +0000

    just using gray_scale is best

--- DataProcessing/faceRecognition.py
+++ DataProcessing/faceRecognition.py
@@ -1,23 +1,38 @@
 import cv2
-import numpy
-import sys
+import numpy as np
 import os
 
-import matplotlib.pyplot as plt
 
-def face_detect(image):
+def face_detect(image, filter):
     # Load the cascade
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     # Read the input image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    filter_image = filter(image)
+    # gray = sobel_filter(gray)
     
     # detect faces
-    face = face_cascade.detectMultiScale(gray, 1.1, 4)
+    face = face_cascade.detectMultiScale(filter_image, 1.1, 4)
     # display the output
     x, y, w, h = face.ravel()
     face_image = image[y:y+h, x:x+w]
     
     return face_image
+
+def gray_scale(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gray
+
+def sobel_filter(img):
+    dx = cv2.Sobel(img, -1, 1, 0, ksize=5)
+    dy = cv2.Sobel(img, -1, 0, 1, ksize=5)
+    
+    abs_dx = cv2.convertScaleAbs(dx)
+    abs_y = cv2.convertScaleAbs(dy)
+    sobel = cv2.addWeighted(abs_dx, 0.5, abs_y, 0.5, 0)
+    return sobel
+
+def canny(img, threshold1=50, threshold2=150):
+    return cv2.Canny(img, threshold1, threshold2)
 
 if __name__=='__main__':
     # load image
