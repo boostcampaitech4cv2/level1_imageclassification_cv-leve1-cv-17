4f52bbbbf915614694bda3c52e3a39799806c73b
mnee15 <rlaflswl0105@naver.com>
2022-10-27 13:05:21 +0000

    set baseline

---
+++ EDA/data_add_label.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import glob
+from collections import defaultdict
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--image-dir-path', help='assign image data dir path')
+parser.add_argument('-c', '--csv-path', help='assign csv data path')
+parser.add_argument('-s', '--save-path', help='assign csv save path')
+args = parser.parse_args()
+
+class DataAddlabel():
+    def __init__(self, image_dir_path : str, csv_path : str, save_path : str):
+        self.image_dir_path = image_dir_path
+        self.csv_path = csv_path
+        self.save_path = save_path
+
+        orig_data = pd.read_csv(self.csv_path)
+        self.orig_columns = orig_data.columns.tolist()
+        self.orig_data = orig_data.values.tolist()
+
+        self.split_age_gender = defaultdict(list)
+        self.labels_dict = defaultdict(list)
+            
+    def data_split_age_gender(self):
+        for data in self.orig_data:
+            age = int(data[3])
+            gender = data[1]
+            age_type = ''
+            label_sqeunce = 0
+            
+            if age < 30:
+                age_type = 'young'
+            elif 30 <= age < 60:
+                age_type = 'middle'
+                label_sqeunce = 1
+            else:
+                age_type = 'old'
+                label_sqeunce = 2
+                
+            self.split_age_gender[f'{label_sqeunce}_{age_type}_{gender}'].append(data)
+
+    def update_labels_dict(self, data_list : list, labels : list):
+        add_label_data = []
+
+        for data in data_list:
+            image_paths = glob.glob(f'{self.image_dir_path}/{data[-1]}/*.jpg')
+
+            for image_path in image_paths:
+                status = image_path.split('/')[-1].split('.')[0]
+                status = re.sub('[0-9]', '', status)
+
+                if status == "mask":
+                    label = labels[0]
+                elif status == "incorrect_mask":
+                    label = labels[1]
+                else:
+                    label = labels[2]
+
+                add_label_data = data + [image_path, label]
+                self.labels_dict[label].append(add_label_data)
+
+    def data_add_label(self):  
+        keys = list(self.split_age_gender.keys())
+        keys.sort()
+
+        male_labels, female_labels = [0, 6, 12], [3, 9, 15]
+
+        for idx, key in enumerate(keys):
+            if idx % 2 == 0:
+                self.update_labels_dict(self.split_age_gender[key], female_labels)
+            else:
+                self.update_labels_dict(self.split_age_gender[key], male_labels)
+                male_labels = [label + 1 for label in male_labels]
+                female_labels = [label + 1 for label in female_labels]
+
+    def save_labeld_data(self):
+        labeld_data = []
+        keys = list(self.labels_dict.keys())
+        keys.sort()
+
+        for key in keys:
+            labeld_data += self.labels_dict[key]
+
+        labeld_data = sorted(labeld_data, key=lambda x: x[0])
+
+        labeld_data_df = pd.DataFrame(labeld_data, columns=self.orig_columns+['image_path', 'label'])
+        labeld_data_df.to_csv(self.save_path, index=False)
+
+
+if __name__ == "__main__":
+    image_dir_path = '/opt/ml/input/data/train/images' if args.image_dir_path == None else args.image_dir_path
+    train_csv_path = '/opt/ml/input/data/train/train.csv' if args.csv_path == None else args.csv_path
+    save_path = '/opt/ml/input/data/train/train_added_label.csv' if args.save_path == None else args.save_path
+
+    dataAddlabel = DataAddlabel(image_dir_path, train_csv_path, save_path)
+    
+    dataAddlabel.data_split_age_gender()
+    dataAddlabel.data_add_label()
+    dataAddlabel.save_labeld_data()
+
+    print('Done.')
---
+++ model.py
@@ -0,0 +1,63 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models as models
+
+class BaseModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=7, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.25)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout2(x)
+
+        x = self.avgpool(x)
+        x = x.view(-1, 128)
+        return self.fc(x)
+
+
+# Custom Model Template
+class EfficientNet_B1(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        """
+        1. 위와 같이 생성자의 parameter 에 num_claases 를 포함해주세요.
+        2. 나만의 모델 아키텍쳐를 디자인 해봅니다.
+        3. 모델의 output_dimension 은 num_classes 로 설정해주세요.
+        """
+        self.backbone = models.efficientnet_b1(weights=models.EfficientNet_B1_Weights.DEFAULT)
+        self.classifier = nn.Linear(1000, num_classes)
+
+        self.name = "EfficientNet_b1"
+
+        self.init_params()
+    def forward(self, x):
+        """
+        1. 위에서 정의한 모델 아키텍쳐를 forward propagation 을 진행해주세요
+        2. 결과로 나온 output 을 return 해주세요
+        """
+        x = self.backbone(x)
+        x = self.classifier(x)
+        return x
+
+    def init_params(self):
+        nn.init.kaiming_uniform_(self.classifier.weight)
+        nn.init.zeros_(self.classifier.bias)
